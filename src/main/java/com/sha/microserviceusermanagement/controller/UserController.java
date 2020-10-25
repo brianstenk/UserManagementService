cf7b409f77dff57336edc4aff744d9d7bab22fb6
Brian Nkurunungi <nkurunungib@gmail.com>
2020-10-24 22:42:48 -0500

    Controller - Rgister/get Names

---
+++ src/main/java/com/sha/microserviceusermanagement/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.sha.microserviceusermanagement.controller;
+
+import com.sha.microserviceusermanagement.model.Role;
+import com.sha.microserviceusermanagement.model.User;
+import com.sha.microserviceusermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    //Authorisation token --
+    @PostMapping("/service/registration")
+    public ResponseEntity<?> saveUser(@RequestBody User user){
+        if(userService.findByUsername(user.getUsername()) != null){
+            //Status Code: 409
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        //Default role = user
+        user.setRole(Role.USER);
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/service/login")
+    public ResponseEntity<?> getUser(Principal principal){
+        //Principal principal = request.getUserPrincipal();
+        if(principal == null || principal.getName() == null){
+            //This means; logout will be successful. login?logout
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        //username = principal.getName()
+        return ResponseEntity.ok(userService.findByUsername(principal.getName()));
+    }
+
+    @PostMapping("/service/names")
+    public ResponseEntity<?> getNamesOfUsers(@RequestBody List<Long> idList){
+        return ResponseEntity.ok(userService.findUsers(idList));
+    }
+
+    @GetMapping("/service/test")
+    public ResponseEntity<?> test(){
+        return ResponseEntity.ok("It is working...");
+    }
+
+}
---
+++ src/main/java/com/sha/microserviceusermanagement/service/UserService.java
@@ -0,0 +1,15 @@
+package com.sha.microserviceusermanagement.service;
+
+import com.sha.microserviceusermanagement.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    User save(User user);
+
+    User findByUsername(String username);
+
+    List<String> findUsers(List<Long> idList);
+}
